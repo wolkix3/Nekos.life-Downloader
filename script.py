a595acd3698e46779a69c6d5218ecca973f5731e
wolkix3 <38121444+wolkix3@users.noreply.github.com>
2018-10-21 06:11:21 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,41 @@
+import requests
+import urllib
+import json
+from collections import namedtuple
+import shutil
+from threading import Thread
+import os
+import multiprocessing
+
+#---------------------------------------------
+#Enter the API URL here:
+api_url = "https://nekos.life/api/v2/img/neko"
+#---------------------------------------------
+
+def neko():
+    while True:
+        filename = ""
+        with requests.Session() as s:
+            api_output = s.get(api_url)
+
+        output = api_output.text
+
+        x = json.loads(output, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+        image_url = x.url
+
+        url = image_url
+        filename = url[url.rfind("/")+1:]
+        exists = os.path.exists(filename)
+
+        if not exists:
+            grab = requests.get(image_url, stream=True)
+            if grab.status_code == 200:
+                with open(filename, "wb") as f:
+                    grab.raw.decode_content = True
+                    shutil.copyfileobj(grab.raw, f)
+            print("Sucessfully downloaded " + filename)
+
+for i in range(multiprocessing.cpu_count()):
+    t = Thread(target=neko)
+    t.start()
